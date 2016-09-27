d59609597faa583f80d5ebd915c3a891243fa3ca
warunee phosri <57160238@go.buu.ac.th>
2016-09-27 14:24:45 +0700

    ScoreA

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -2,42 +2,50 @@ function Tennis(){
 
     var A;
     var B;
-
   this.start=function(){
     A=0;
     B=0;
 
+  }
+
+  this.ScoreA=function(){
+if(A===0)
+{
+  A=15;
+}
+else if(A===15)
+{
+  A=30;
+}
+else if(A===30)
+{
+  A=40;
+}
+else if(A===40)
+{
+  A=50;
+}
 
   }
 
-  this.ScoreB=function(){
-if(B===0)
-{
-  B=15;
-
-}
-else if(B===15)
-{
-  B=30;
-
-}
-else if(B===30)
-{
-  B=40;
-
-}
-else if(B===40)
-{
-  B=50;
-
-}
-
-  }
 
 this.echo = function(){
   if(A===0&&B===0){
       return'Love-Love';
 
+  }
+  if(A===15&&B===0){
+    return'Fifteen-Love'
+  }
+  if(A===30&&B===0){
+    return'Thirty-Love'
+  }
+
+  if(A===40&&B===0){
+  return'forty-Love'
+  }
+  if(A===50&&B===0){
+  return'A winner'
   }
 
 }
@@ -51,6 +59,35 @@ it('should echo "Love-Love"when starting the game',function(){
 tennis.start();
 
 expect(tennis.echo()).toEqual('Love-Love');
+})
+
+
+it('should echo "Fifteen-Love"when A Rated',function(){
+
+tennis.ScoreA();
+
+expect(tennis.echo()).toEqual('Fifteen-Love');
+})
+
+it('should echo "Thirty-Love"when A Rated',function(){
+
+tennis.ScoreA();
+
+expect(tennis.echo()).toEqual('Thirty-Love');
+})
+
+it('should echo "forty-Love"when A Rated',function(){
+
+tennis.ScoreA();
+
+expect(tennis.echo()).toEqual('forty-Love');
+})
+it('should echo "A winner"when A winner',function(){
+
+
+tennis.ScoreA();
+
+expect(tennis.echo()).toEqual('A winner');
 })
 
 });
