559d35292a487b07730c5638180e5af1cf93576e
setchomaru <57160339@go.buu.ac.th>
2016-09-28 00:30:42 +0700

    B=15-0

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -31,27 +31,52 @@ else if(A===40)
   A=50;
 
 }
+}
+
+
+  this.ScoreB=function(){
+
+  if(B===0){
+    B=15;
+  }
+  else if(B===15)
+  {
+    B=30;
+  }
+  else if(B===30)
+  {
+    B=40;
 
   }
+  else if(B===40)
+  {
+    B=50;
+
+  }
+}
 
 this.echo = function(){
   if(A===0&&B===0){
       return'Love-Love';
-
-  }
-  if(A===15&&B===0){
+}
+else if(A===15&&B===0){
     return'Fifteen-Love'
   }
-  if(A===30&&B===0){
+   else if(A===30&&B===0){
     return'Thirty-Love'
   }
 
-  if(A===40&&B===0){
+ else if(A===40&&B===0){
   return'forty-Love'
   }
-  if(A===50&&B===0){
+ else if(A===50&&B===0){
   return'A Winner'
   }
+else if(B===15)
+{
+    return'Fifteen-Love'
+}
+
 
 }
 }
@@ -63,7 +88,7 @@ it('should echo "Love-Love"when starting the game',function(){
 
 tennis.start();
 
-expect(tennis.echo()).toEqual('Love-Love');
+expect(tennis.echo()).toEqual('Love-Love'); //A=0-0
 })
 
 
@@ -71,21 +96,21 @@ it('should echo "Fifteen-Love"when Get ScoreA',function(){
 
 tennis.ScoreA();
 
-expect(tennis.echo()).toEqual('Fifteen-Love');
+expect(tennis.echo()).toEqual('Fifteen-Love');//A=15-0
 })
 
 it('should echo "Thirty-Love"when Get ScoreA',function(){
 
 tennis.ScoreA();
 
-expect(tennis.echo()).toEqual('Thirty-Love');
+expect(tennis.echo()).toEqual('Thirty-Love');//A=30-0
 })
 
 it('should echo "forty-Love"when Get ScoreA',function(){
 
 tennis.ScoreA();
 
-expect(tennis.echo()).toEqual('forty-Love');
+expect(tennis.echo()).toEqual('forty-Love');//A=40-0
 })
 
 it('should echo "A Winner "when A Winner',function(){
@@ -93,13 +118,15 @@ it('should echo "A Winner "when A Winner',function(){
 
 tennis.ScoreA();
 
-expect(tennis.echo()).toEqual('A Winner');
+expect(tennis.echo()).toEqual('A Winner'); //A win
 })
-it('should echo "B Winner "when A Winner',function(){
+
+it('should echo "Fifteen-Love"when  Get ScoreB',function(){
 
 
 tennis.ScoreB();
 
-expect(tennis.echo()).toEqual('B Winner');
-})  //เจ๋งมากค่ะ 
+expect(tennis.echo()).toEqual('Fifteen-Love'); //B= 15-0
+})
+
 });
