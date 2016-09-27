042ce17300a371389c9ae79af754a3f6e3ebf355
setchomaru <57160339@go.buu.ac.th>
2016-09-27 07:49:38 +0700

    A winner

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,14 +1,6 @@
-function Tennis(){
-    this.start  = function () {
-    };
-    this.echo = function(){
-      return 'Love - Love';
-    };
-};
 
-describe('Tennis game', function(){
-  it('Should echo "Love - Love" when starting the game', function(){
-    var tennis = new Tennis(0,0);
-    expect(tennis.echo()).toEqual('Love - Love');
+describe('Tennis', function () {
+  it('should be pass', function () {
+    expect(true).toBe(true);
   });
-  });
+});
