a342cfb9a89d1cdee402e474615d3e151a364865
warunee phosri <57160238@go.buu.ac.th>
2016-09-27 13:51:59 +0700

    ScoreB0-0

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -10,25 +10,25 @@ function Tennis(){
 
   }
 
-  this.ScoreA=function(){
-if(A===0)
+  this.ScoreB=function(){
+if(B===0)
 {
-  A=15;
+  B=15;
 
 }
-else if(A===15)
+else if(B===15)
 {
-  A=30;
+  B=30;
 
 }
-else if(A===30)
+else if(B===30)
 {
-  A=40;
+  B=40;
 
 }
-else if(A===40)
+else if(B===40)
 {
-  A=50;
+  B=50;
 
 }
 
@@ -38,19 +38,6 @@ this.echo = function(){
   if(A===0&&B===0){
       return'Love-Love';
 
-  }
-  if(A===15&&B===0){
-    return'Fifteen-Love'
-  }
-  if(A===30&&B===0){
-    return'Thirty-Love'
-  }
-
-  if(A===40&&B===0){
-  return'forty-Love'
-  }
-  if(A===50&&B===0){
-  return'A Winner'
   }
 
 }
@@ -66,41 +53,4 @@ tennis.start();
 expect(tennis.echo()).toEqual('Love-Love');
 })
 
-
-it('should echo "Fifteen-Love"when Get ScoreA',function(){
-
-tennis.ScoreA();
-
-expect(tennis.echo()).toEqual('Fifteen-Love');
-})
-
-it('should echo "Thirty-Love"when Get ScoreA',function(){
-
-tennis.ScoreA();
-
-expect(tennis.echo()).toEqual('Thirty-Love');
-})
-
-it('should echo "forty-Love"when Get ScoreA',function(){
-
-tennis.ScoreA();
-
-expect(tennis.echo()).toEqual('forty-Love');
-})
-
-it('should echo "A Winner "when A Winner',function(){
-
-
-tennis.ScoreA();
-
-expect(tennis.echo()).toEqual('A Winner');
-})
-it('should echo "B Winner "when A Winner',function(){
-
-
-tennis.ScoreB();
-
-expect(tennis.echo()).toEqual('B Winner');
-})  //เจ๋งมากค่ะ
-//ขอบคุณคะ
 });
