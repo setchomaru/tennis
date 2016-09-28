91e84778392bc33e77ff1f6474a4c4c6e71c92ae
warunee phosri <57160238@go.buu.ac.th>
2016-09-29 00:05:31 +0700

    B=50

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -39,91 +39,32 @@ this.ScoreB=function(){
     B=30;
     }
     else if(B===30)
-{
-B=40;
-}
-}
-
-<<<<<<< HEAD
-this.echoB = function(){
-  if(A===0&&B===15){
-    return'Love-Fifteen';
-=======
-<<<<<<< HEAD
-}
-//ScoreB
-this.ScoreB=function(){
-if(B===0)
-{
-B=15;
-}
-else if(B===15)
-{
-B=30;
-}
-
-=======
->>>>>>> d2d9d8c6028fbdea1d37d6371d5d4e611ba5062b
-  }
-  if(A===0&&B===30){
-    return'Love-Thirty';
-  }
->>>>>>> 3fb3edc74e23c2f4e021b3fd1601c0706c1b936a
-
-if(A===0&&B===40){
-  return'Love-Forty';
-  }
+    {
+      B=40;
+    }
+    else if(B===40)
+    {
+      B=50;
+    }
 }
 
 this.echo = function(){
-<<<<<<< HEAD
-if(A===0&&B===0){
-    return'Love-Love';
 
-}
-if(A===15&&B===0){
-  return'Fifteen-Love';
-}
-if(A===30&&B===0){
-  return'Thirty-Love';
-}
-=======
-  if(A===0&&B===0){
+if(A===0&&B===0){
       return'Love-Love';
 }
 else if(A===15&&B===0){
     return'Fifteen-Love'
   }
-   else if(A===30&&B===0){
+else if(A===30&&B===0){
     return'Thirty-Love'
   }
 
- else if(A===40&&B===0){
-  return'forty-Love'
-  }
-
- else if(A===50&&B===0){
-  return'A Winner'
-<<<<<<< HEAD
-=======
-=======
-  if(A===50&&B===0){
-  return'A winner'
->>>>>>> d59609597faa583f80d5ebd915c3a891243fa3ca
-  }
-else if(B===15)
-{
-    return'Fifteen-Love'
-}
-
->>>>>>> 3fb3edc74e23c2f4e021b3fd1601c0706c1b936a
->>>>>>> d2d9d8c6028fbdea1d37d6371d5d4e611ba5062b
-
 if(A===40&&B===0){
-return'forty-Love';
+  return'forty-Love';
 }
 if(A===50&&B===0){
-return'A winner';
+  return'A winner';
 }
 }
 //ScoreB
@@ -137,6 +78,12 @@ if(A===0&&B===15){
 if(A===0&&B===30){
   return'Love-Thirty';
 }
+if(A===0&&B===40){
+ return'Love-Forty';
+ }
+ if(A===0&&B===50){
+ return'B winner';
+ }
 }
 }
 
@@ -174,22 +121,10 @@ it('should echo "A winner"when A winner',function(){
 
 tennis.ScoreA();
 
-expect(tennis.echo()).toEqual('A Winner'); //A win
+expect(tennis.echo()).toEqual('A winner'); //A win
 })
 
 });
-
-describe('tennis game',function(){
-  var tennis = new Tennis();
-it('should echo "Love-Fifteen"when  Get ScoreB',function(){
-
-
-tennis.ScoreB();
-
-expect(tennis.echoB()).toEqual('Love-Fifteen'); //B== 15-0
-})
-});
-
 
 //ScoreB
 
@@ -219,4 +154,11 @@ tennisB.ScoreB();
 
 expect(tennisB.echoB()).toEqual('Love-Forty'); // B=0-40
 })
+it('should echo "B winner" when B winner',function(){
+
+tennisB.ScoreB();
+
+expect(tennisB.echoB()).toEqual('B winner');  //B=50
+})
+
 });
